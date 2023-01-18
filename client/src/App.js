e792a5d235bcfd0f241f8b93b811c571a6bf54da
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-01-18 16:38:57 -0300

    Inicio Crud usando react, mysql e node

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app--container">
+      <div className="register--container">
+        <h1>Scrim Shop</h1>
+        <input  type="text" name="name" placeholder="Nome"  className="register--input" />
+
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default App;
