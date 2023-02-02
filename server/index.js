2bc17944eb2c4807327555258ab22e07c55c88c1
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-02-01 22:24:02 -0300

    Adicionando post que registar no db

--- server/index.js
+++ server/index.js
@@ -10,19 +10,22 @@ const db = mysql.createPool({
     database: "crudgames",
 });
 
-
-//  app.get('/', (req, res) => {
-//     let SQL = "INSERT INTO games ( name, cost, category ) VALUES ( 'Far Cry 5', '120', 'Ação' ) ";
-
-//     db.query(SQL, (err, result) => {
-//         console.log(err);
-//     })
-
-//  });   
-
+  
 app.use(cors());
 app.use(express.json());
 
+app.post("/register", (req, res) => {
+    const { name } = req.body;
+    const { cost } = req.body;
+    const { category } = req.body;
+
+    let SQL = " INSERT INTO games ( name, cost, category ) VALUES ( ?, ?, ? ) ";
+
+    db.query(SQL, [name, cost, category], (err, result) => {
+        console.log(err);
+    });    
+});
+
 app.listen(3001, () => {
     console.log("Rodando serverno")
-})
+});
