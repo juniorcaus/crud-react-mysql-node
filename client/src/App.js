18a78417164bce4607e084e5ae6589616719094e
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-02-01 22:17:17 -0300

    Adicionando Axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import Axios from "axios";
 import './App.css'; 
 
 function App() {
@@ -14,8 +14,14 @@ function App() {
   };
 
   const handleClickButton = () => {
-    console.log(values);
-  }
+    Axios.post("http://localhost:3001/register", {
+      name: values.name,
+      cost: values.cost,
+      category: values.category
+    }).then((response) => {
+      console.log(response);
+    });
+  };
  
   return (
     <div className="app-container">
