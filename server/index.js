62f82a6a85be217ae3c6a8cc7b55cf0f5e203388
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-02-01 22:07:00 -0300

    Inicioando e testando servidor

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
 
- app.get('/', (req, res) => {
-    res.send("Hello world");
- });   
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "324471",
+    database: "crudgames",
+});
 
+
+//  app.get('/', (req, res) => {
+//     let SQL = "INSERT INTO games ( name, cost, category ) VALUES ( 'Far Cry 5', '120', 'Ação' ) ";
+
+//     db.query(SQL, (err, result) => {
+//         console.log(err);
+//     })
+
+//  });   
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(3001, () => {
     console.log("Rodando serverno")
